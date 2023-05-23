e0b9b410765e63a3d19cad458639439fa7be618b
Bruninha <Brunanotube@gmail.com>
2023-05-23 20:40:07 -0300

    Configura por-evento

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+function setup(){
+window,addEventListener('load', setup);
+
+
 const form= document.forms.entrada;
         form.addEventListener('submit', envia);
+ }
     
         function envia(evento){
             evento.preventDefalt();
