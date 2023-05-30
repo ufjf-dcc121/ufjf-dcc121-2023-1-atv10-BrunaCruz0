f531205d5d9d6e5f1e2fde2832e3db3135233ff8
Bruninha <Brunanotube@gmail.com>
2023-05-30 19:36:34 -0300

    Adiciona estado basico

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-window.addEventListener('load', setup);
+const form = document.forms.entrada;
+form.addEventListener('submit', envia);
 
-function setup() {
-    const form = document.forms.entrada;
-    form.addEventListener('submit', envia);
-}
+
+let estado = 0;
+atualiza();
 
 function envia(evento) {
     evento.preventDefault();
     console.log('Formulario enviado!');
+    estado++;
+    atualiza();
+}
+function atualiza() {
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
 }
